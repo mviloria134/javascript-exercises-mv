a0ab7687ea3c600e2e6e0bdc4c9e08dbf3b52ed5
Maria Viloria <mviloria134@gmail.com>
2026-09-03 01:31:44 -0700

    Complete foundations 09

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (!Number.isInteger(a) || !Number.isInteger(b) || a < 0 || b < 0) {
+        return "ERROR";
+    }
 
+    let sum = 0;
+    let larger;
+    let smaller;
+    if (a > b) {
+        larger = a;
+        smaller = b;
+    }
+    else {
+        larger = b;
+        smaller = a;
+    }
+
+    for (let i = smaller; i <= larger; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
