53c7fab623321789406660eeb24eadff734229d2
Maria Viloria <mviloria134@gmail.com>
2026-09-14 17:35:34 -0700

    Complete object_basics 05

--- foundations/object_basics/05_findTheOldest/findTheOldest.js
+++ foundations/object_basics/05_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
+function getAge(person) {
+    return (person.yearOfDeath) ? 
+        person.yearOfDeath - person.yearOfBirth : 
+        new Date().getFullYear() - person.yearOfBirth;
+}
 
+const findTheOldest = function(people) {
+    return people.reduce((oldest, person) => {
+        const age = getAge(person);
+        const oldestAge = getAge(oldest);
+        return (age > oldestAge) ? person : oldest;
+    });
 };
 
 // Do not edit below this line
