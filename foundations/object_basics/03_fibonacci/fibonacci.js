2f5fbd2dc5352b19e85b2d6669b887e7622f116a
Maria Viloria <mviloria134@gmail.com>
2026-09-14 17:05:27 -0700

    Complete object_basics 03

--- foundations/object_basics/03_fibonacci/fibonacci.js
+++ foundations/object_basics/03_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+    if (index < 0) return "OOPS";
+    if (index == 0) return 0;
 
+    let prevNum = 0;
+    let currentNum = 1;
+
+    for (let i = 1; i < index; i++) {
+        [prevNum, currentNum] = [currentNum, currentNum+prevNum];
+    }
+
+    return currentNum;
 };
 
 // Do not edit below this line
